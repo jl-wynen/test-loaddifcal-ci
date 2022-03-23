f9af4411a7d5931a0281d974f45bf1f845442be7
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2022-03-23 11:04:05 +0100

    Add initial test setup

---
+++ test.py
@@ -0,0 +1,26 @@
+from mantid.simpleapi import DeleteWorkspace, LoadDiffCal
+from mantid.api import AnalysisDataService
+import pooch
+
+
+def get_file():
+    return pooch.create(
+        path=pooch.os_cache('test-loaddiffcal'),
+        base_url='https://public.esss.dk/groups/scipp/ess/powgen/1/',
+        registry={
+            'PG3_FERNS_d4832_2011_08_24.cal': 'md5:c181221ebef9fcf30114954268c7a6b6'
+        }).fetch('PG3_FERNS_d4832_2011_08_24.cal')
+
+
+def main():
+    ws = LoadDiffCal(Filename=str(get_file()),
+                     InstrumentFilename='POWGEN_Definition_2011-02-25.xml',
+                     WorkspaceName='ws',
+                     MakeGroupingWorkspace=False)
+    for name in AnalysisDataService.Instance().getObjectNames():
+        if name.startswith('ws'):
+            DeleteWorkspace(name)
+
+
+if __name__ == '__main__':
+  main()
